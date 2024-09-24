f0afb228289ddbf495843464a8ee0f19b10df376
James Joy <joyja@joyautomation.com>
2024-09-24 15:34:55 -0700

    cleanup

--- server.ts
+++ server.ts
@@ -3,19 +3,14 @@ import { validateHost, validatePort } from "./validation.ts";
 import { addHistoryEvents, addHostToSchema, getHost } from "./synapse.ts";
 import { log } from "./log.ts";
 import type { Args } from "@std/cli";
-import SchemaBuilder from "@pothos/core";
 import { setLogLevel } from "@joyautomation/coral";
-import { type Db, getDb } from "./db/db.ts";
-import { getBuilder, initialize } from "./db/graphql.ts";
+import { getDb } from "./db/db.ts";
+import { getBuilder } from "./db/graphql.ts";
 
 export const _internal = {
   getDb,
   getHost,
 };
-
-const flatten =
-  <T extends { [id: string]: any }>(key: keyof T) =>
-  (parent: T): T[keyof T][] => Object.values(parent[key]);
 
 export async function runServer(args: Args) {
   setLogLevel(
