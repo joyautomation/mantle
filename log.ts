09364d20b1cd7db13d8170fdb98f63dda5011cbf
James A. Joy, PE <38839768+joyja@users.noreply.github.com>
2024-09-30 08:58:12 -0700

    updated log (#17)
    * updated log
    
    * Bump version to 0.0.21
    
    ---------
    
    Co-authored-by: James Joy <joyja@joyautomation.com>

--- log.ts
+++ log.ts
@@ -1,3 +1,11 @@
 import { createLogger, LogLevel } from "@joyautomation/coral";
+import { logs as synapse } from "@joyautomation/synapse";
 
-export const log = createLogger("mantle", LogLevel.debug);
+export const log = createLogger("mantle", LogLevel.info);
+
+export const logs = {
+  synapse,
+  mantle: {
+    main: log,
+  },
+};
