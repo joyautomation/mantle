f7dd0eff8044ecb16f084270861ecb76e3dbcafe
James A. Joy, PE <38839768+joyja@users.noreply.github.com>
2025-03-07 21:44:36 -0800

    added timetamp and metricId (#39)
    Co-authored-by: James Joy <joyja@joyautomation.com>

--- synapse.ts
+++ synapse.ts
@@ -31,7 +31,8 @@ import Long from "long";
  */
 export function getHost(args: Args) {
   const config: SparkplugCreateHostInput = {
-    brokerUrl: args.brokerUrl ||
+    brokerUrl:
+      args.brokerUrl ||
       Deno.env.get("MANTLE_MQTT_BROKER_URL") ||
       "ssl://mqtt3.anywherescada.com:8883",
     username: args.username || Deno.env.get("MANTLE_MQTT_USERNAME") || "",
@@ -41,11 +42,12 @@ export function getHost(args: Args) {
       args.clientId || Deno.env.get("MANTLE_MQTT_CLIENT_ID") || "mantle"
     }-${nanoid(7)}`,
     version: "spBv1.0",
-    primaryHostId: args.primaryHostId ||
+    primaryHostId:
+      args.primaryHostId ||
       Deno.env.get("MANTLE_MQTT_PRIMARY_HOST_ID") ||
       "test",
-    sharedSubscriptionGroup: args.sharedSubscriptionGroup ||
-      Deno.env.get("MANTLE_SHARED_GROUP"),
+    sharedSubscriptionGroup:
+      args.sharedSubscriptionGroup || Deno.env.get("MANTLE_SHARED_GROUP"),
   };
   return createHost(config);
 }
@@ -66,7 +68,7 @@ export function addHistoryEvents(
   db: Db,
   host: SparkplugHost,
   publisher?: ReturnType<typeof createClient>,
-  subscriber?: ReturnType<typeof createClient>,
+  subscriber?: ReturnType<typeof createClient>
 ) {
   ["nbirth", "dbirth", "ndata", "ddata"].forEach((topic) => {
     host.events.on(topic, async (topic: SparkplugTopic, message: UPayload) => {
@@ -86,9 +88,9 @@ export function addHistoryEvents(
                 ...metric,
                 timestamp: convertIfLong(metric.timestamp),
                 value: convertIfLong(metric.value),
-              }),
+              })
             );
-          }) || [],
+          }) || []
         );
       } else {
         pubsub.publish(
@@ -99,7 +101,7 @@ export function addHistoryEvents(
             nodeId: topic.edgeNode,
             deviceId: topic.deviceId,
             metricId: metric.name,
-          })),
+          }))
         );
       }
     });
@@ -114,23 +116,18 @@ export function addHistoryEvents(
 export function addHostToSchema(
   host: SparkplugHost,
   builder: ReturnType<typeof getBuilder>,
-  redis?: ReturnType<typeof createClient>,
+  redis?: ReturnType<typeof createClient>
 ) {
-  const SparkplugGroupRef = builder.objectRef<SparkplugGroupFlat>(
-    "SparkplugGroup",
-  );
-  const SparkplugNodeRef = builder.objectRef<SparkplugNodeFlat>(
-    "SparkplugNode",
-  );
-  const SparkplugDeviceRef = builder.objectRef<SparkplugDeviceFlat>(
-    "SparkplugDevice",
-  );
-  const SparkplugMetricRef = builder.objectRef<SparkplugMetricFlat>(
-    "SparkplugMetric",
-  );
-  const SparkplugMetricPropertyRef = builder.objectRef<
-    SparkplugMetricPropertyFlat
-  >("SparkplugMetricProperty");
+  const SparkplugGroupRef =
+    builder.objectRef<SparkplugGroupFlat>("SparkplugGroup");
+  const SparkplugNodeRef =
+    builder.objectRef<SparkplugNodeFlat>("SparkplugNode");
+  const SparkplugDeviceRef =
+    builder.objectRef<SparkplugDeviceFlat>("SparkplugDevice");
+  const SparkplugMetricRef =
+    builder.objectRef<SparkplugMetricFlat>("SparkplugMetric");
+  const SparkplugMetricPropertyRef =
+    builder.objectRef<SparkplugMetricPropertyFlat>("SparkplugMetricProperty");
 
   type SparkplugMetricUpdate = SparkplugMetric & {
     groupId: string;
@@ -138,7 +135,7 @@ export function addHostToSchema(
     deviceId: string;
   };
   const SparkplugMetricUpdateRef = builder.objectRef<SparkplugMetricUpdate>(
-    "SparkplugMetricUpdate",
+    "SparkplugMetricUpdate"
   );
 
   SparkplugGroupRef.implement({
@@ -222,11 +219,13 @@ export function addHostToSchema(
           return flattenHostGroups(host);
         }
       },
-    }));
+    })
+  );
   builder.subscriptionField("metricUpdate", (t) =>
     t.field({
       type: [SparkplugMetricUpdateRef],
       subscribe: () => pubsub.subscribe("metricUpdate"),
       resolve: (payload) => payload,
-    }));
+    })
+  );
 }
