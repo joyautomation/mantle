89f55aa8803594a2823ff6c82d73166bd3db6163
James A Joy <joyja@mira1.internal1.jarautomation.io>
2025-05-16 14:06:20 -0700

    added statisticalHistory

--- history.ts
+++ history.ts
@@ -68,7 +68,7 @@ export function getValueType(metric: SparkplugMetric) {
  * @returns {Date | null} The calculated Date object or null if input is invalid.
  */
 export function calcTimestamp(
-  timestamp?: Long.Long | number | null,
+  timestamp?: Long.Long | number | null
 ): Date | null {
   if (timestamp) {
     let timestampMs: number;
@@ -96,7 +96,7 @@ export function calcTimestamp(
 export async function recordValues(
   db: Db,
   topic: SparkplugTopic,
-  message: UPayload,
+  message: UPayload
 ) {
   const { metrics } = message;
   const { groupId, edgeNode: nodeId, deviceId } = topic;
@@ -106,7 +106,7 @@ export async function recordValues(
       const valueType = getValueType(metric);
       if (metric.name && timestamp) {
         log.debug(
-          `Recording metric: ${metric.name} with value: ${metric.value}`,
+          `Recording metric: ${metric.name} with value: ${metric.value}`
         );
         const record: HistoryRecord = {
           groupId,
@@ -114,24 +114,21 @@ export async function recordValues(
           metricId: metric.name || "",
           deviceId: deviceId || null,
           timestamp,
-          intValue: valueType === "intValue"
-            ? Long.isLong(metric.value)
-              ? metric.value.toNumber()
-              : (metric.value as number)
-            : null,
-          floatValue: valueType === "floatValue"
-            ? (metric.value as number)
-            : null,
+          intValue:
+            valueType === "intValue"
+              ? Long.isLong(metric.value)
+                ? metric.value.toNumber()
+                : (metric.value as number)
+              : null,
+          floatValue:
+            valueType === "floatValue" ? (metric.value as number) : null,
           stringValue: valueType === "stringValue" ? `${metric.value}` : null,
-          boolValue: valueType === "boolValue"
-            ? (metric.value as boolean)
-            : null,
+          boolValue:
+            valueType === "boolValue" ? (metric.value as boolean) : null,
         };
-        for (
-          const [propertyKey, property] of Object.entries(
-            metric.properties || {},
-          )
-        ) {
+        for (const [propertyKey, property] of Object.entries(
+          metric.properties || {}
+        )) {
           const valueType = getPropertyType(property);
           const propertyRecord: HistoryPropertyRecord = {
             groupId,
@@ -140,27 +137,25 @@ export async function recordValues(
             metricId: metric.name || "",
             timestamp,
             propertyId: propertyKey,
-            intValue: valueType === "intValue"
-              ? Long.isLong(property.value)
-                ? property.value.toNumber()
-                : (property.value as number)
-              : null,
-            floatValue: valueType === "floatValue"
-              ? (property.value as number)
-              : null,
-            stringValue: valueType === "stringValue"
-              ? `${property.value}`
-              : null,
-            boolValue: valueType === "boolValue"
-              ? (property.value as boolean)
-              : null,
+            intValue:
+              valueType === "intValue"
+                ? Long.isLong(property.value)
+                  ? property.value.toNumber()
+                  : (property.value as number)
+                : null,
+            floatValue:
+              valueType === "floatValue" ? (property.value as number) : null,
+            stringValue:
+              valueType === "stringValue" ? `${property.value}` : null,
+            boolValue:
+              valueType === "boolValue" ? (property.value as boolean) : null,
           };
           try {
             await db.insert(historyPropertiesTable).values(propertyRecord);
           } catch (error) {
             log.error(
               `Error recording metric property: ${metric.name}.${propertyKey}`,
-              error,
+              error
             );
           }
         }
@@ -171,7 +166,7 @@ export async function recordValues(
         }
       } else {
         log.warn(
-          `Metric missing name or timestamp: name: ${metric.name}, timestamp: ${metric.timestamp}`,
+          `Metric missing name or timestamp: name: ${metric.name}, timestamp: ${metric.timestamp}`
         );
       }
     }
@@ -183,12 +178,31 @@ type MetricHistory = {
   timestamp: Date;
 };
 
+type StatisticalMetricHistory = {
+  value: string | null;
+  avg: string | null;
+  max: string | null;
+  min: string | null;
+  stddev: string | null;
+  sum: string | null;
+  count: string | null;
+  timestamp: Date;
+};
+
 type History = {
   groupId: string;
   nodeId: string;
   deviceId: string | null;
   metricId: string;
   history: MetricHistory[];
+};
+
+type StatisticalHistory = {
+  groupId: string;
+  nodeId: string;
+  deviceId: string | null;
+  metricId: string;
+  history: StatisticalMetricHistory[];
 };
 
 type HistoryMetricInput = {
@@ -216,16 +230,15 @@ export async function getHistory({
   db: Db;
 }) {
   try {
-    const autoInterval = `${
-      Math.floor(
-        (end.getTime() - start.getTime()) / (1000 * (samples ?? 100)),
-      )
-    }s`;
-    const time = raw == null
-      ? sql<Date>`${historyTable.timestamp} as "time"`
-      : sql<Date>`time_bucket(${
-        interval ?? autoInterval
-      }, "timestamp") as "time"`;
+    const autoInterval = `${Math.floor(
+      (end.getTime() - start.getTime()) / (1000 * (samples ?? 100))
+    )}s`;
+    const time =
+      raw == null
+        ? sql<Date>`${historyTable.timestamp} as "time"`
+        : sql<Date>`time_bucket(${
+            interval ?? autoInterval
+          }, "timestamp") as "time"`;
     const normalizedMetrics = metrics.map((metric) => ({
       groupId: String(metric.groupId),
       nodeId: String(metric.nodeId),
@@ -235,26 +248,22 @@ export async function getHistory({
     const subQuery = db
       .select({
         time,
-        name: sql<
-          string
-        >`CONCAT("group_id",'/',"node_id",'/',"device_id",'/',"metric_id") as "name"`,
+        name: sql<string>`CONCAT("group_id",'/',"node_id",'/',"device_id",'/',"metric_id") as "name"`,
         value: sql<number>`AVG("float_value") as "value"`,
       })
       .from(historyTable)
       .where(
         and(
           sql.raw(
-            `("group_id", "node_id", "device_id", "metric_id") in (${
-              metrics
-                .map(
-                  (m) =>
-                    `('${m.groupId}', '${m.nodeId}', '${m.deviceId}', '${m.metricId}')`,
-                )
-                .join(", ")
-            })`,
+            `("group_id", "node_id", "device_id", "metric_id") in (${metrics
+              .map(
+                (m) =>
+                  `('${m.groupId}', '${m.nodeId}', '${m.deviceId}', '${m.metricId}')`
+              )
+              .join(", ")})`
           ),
-          between(historyTable.timestamp, start, end),
-        ),
+          between(historyTable.timestamp, start, end)
+        )
       )
       .groupBy(sql`time`, sql`name`)
       .orderBy(sql`time asc`);
@@ -273,14 +282,125 @@ export async function getHistory({
             .map((h) => {
               return {
                 timestamp: new Date(h.time),
-                value: h.data[
+                value:
+                  h.data[
+                    `${m.groupId}/${m.nodeId}/${m.deviceId}/${m.metricId}`
+                  ],
+              };
+            })
+            .filter((h) => h.value !== null && h.value !== undefined),
+        };
+      })
+    );
+  } catch (error) {
+    return createFail(createErrorString(error));
+  }
+}
+
+export async function getStatisticalHistory({
+  db,
+  metrics,
+  start,
+  end,
+  interval,
+  samples,
+  raw,
+}: {
+  metrics: HistoryMetricInput[];
+  start: Date;
+  end: Date;
+  interval?: string | null;
+  samples?: number | null;
+  raw?: boolean | null;
+  db: Db;
+}) {
+  try {
+    const autoInterval = `${Math.floor(
+      (end.getTime() - start.getTime()) / (1000 * (samples ?? 100))
+    )}s`;
+    const time =
+      raw == null
+        ? sql<Date>`${historyTable.timestamp} as "time"`
+        : sql<Date>`time_bucket(${
+            interval ?? autoInterval
+          }, "timestamp") as "time"`;
+    const normalizedMetrics = metrics.map((metric) => ({
+      groupId: String(metric.groupId),
+      nodeId: String(metric.nodeId),
+      deviceId: metric.deviceId ? String(metric.deviceId) : null,
+      metricId: String(metric.metricId),
+    }));
+    const subQuery = db
+      .select({
+        time,
+        name: sql<string>`CONCAT("group_id",'/',"node_id",'/',"device_id",'/',"metric_id") as "name"`,
+        avg: sql<number>`AVG("float_value") as "avg"`,
+        max: sql<number>`MAX("float_value") as "max"`,
+        min: sql<number>`MIN("float_value") as "min"`,
+        stddev: sql<number>`STDDEV("float_value") as "stddev"`,
+        sum: sql<number>`SUM("float_value") as "sum"`,
+        count: sql<number>`COUNT("float_value") as "count"`,
+      })
+      .from(historyTable)
+      .where(
+        and(
+          sql.raw(
+            `("group_id", "node_id", "device_id", "metric_id") in (${metrics
+              .map(
+                (m) =>
+                  `('${m.groupId}', '${m.nodeId}', '${m.deviceId}', '${m.metricId}')`
+              )
+              .join(", ")})`
+          ),
+          between(historyTable.timestamp, start, end)
+        )
+      )
+      .groupBy(sql`time`, sql`name`)
+      .orderBy(sql`time asc`);
+    const history = await db
+      .select({
+        time: sql<Date>`"time"`,
+        data: sql<
+          Record<
+            string,
+            {
+              value: string;
+              avg: string;
+              max: string;
+              min: string;
+              stddev: string;
+              sum: string;
+              count: string;
+            }
+          >
+        >`json_object_agg("name",json_build_object(
+            'value', "avg",
+            'avg', "avg",
+            'max', "max",
+            'min', "min",
+            'stddev', "stddev",
+            'sum', "sum",
+            'count', "count"
+          ))`,
+      })
+      .from(sql`${subQuery} as bucketed`)
+      .groupBy(sql`time`);
+    return createSuccess(
+      normalizedMetrics.map((m) => {
+        return {
+          ...m,
+          history: history
+            .map((h) => {
+              return {
+                timestamp: new Date(h.time),
+                ...h.data[
                   `${m.groupId}/${m.nodeId}/${m.deviceId}/${m.metricId}`
                 ],
               };
             })
             .filter((h) => h.value !== null && h.value !== undefined),
         };
-      }),
+      })
     );
   } catch (error) {
     return createFail(createErrorString(error));
@@ -289,13 +409,31 @@ export async function getHistory({
 
 export function addHistoryToSchema(
   builder: ReturnType<typeof getBuilder>,
-  db: Db,
+  db: Db
 ) {
   const MetricHistoryRef = builder.objectRef<MetricHistory>("MetricHistory");
+  const StatisticalMetricHistoryRef =
+    builder.objectRef<StatisticalMetricHistory>("StatisticalMetricHistory");
   const HistoryRef = builder.objectRef<History>("History");
   MetricHistoryRef.implement({
     fields: (t) => ({
       value: t.exposeString("value"),
+      timestamp: t.expose("timestamp", {
+        type: "Date",
+      }),
+    }),
+  });
+  const StatisticalHistoryRef =
+    builder.objectRef<StatisticalHistory>("StatisticalHistory");
+  StatisticalMetricHistoryRef.implement({
+    fields: (t) => ({
+      value: t.exposeString("value"),
+      avg: t.exposeString("avg"),
+      max: t.exposeString("max"),
+      min: t.exposeString("min"),
+      stddev: t.exposeString("stddev"),
+      sum: t.exposeString("sum"),
+      count: t.exposeString("count"),
       timestamp: t.expose("timestamp", {
         type: "Date",
       }),
@@ -309,6 +447,18 @@ export function addHistoryToSchema(
       metricId: t.exposeString("metricId"),
       history: t.field({
         type: [MetricHistoryRef],
+        resolve: (parent) => parent.history,
+      }),
+    }),
+  });
+  StatisticalHistoryRef.implement({
+    fields: (t) => ({
+      groupId: t.exposeString("groupId"),
+      nodeId: t.exposeString("nodeId"),
+      deviceId: t.exposeString("deviceId"),
+      metricId: t.exposeString("metricId"),
+      history: t.field({
+        type: [StatisticalMetricHistoryRef],
         resolve: (parent) => parent.history,
       }),
     }),
@@ -349,5 +499,36 @@ export function addHistoryToSchema(
           throw new GraphQLError(result.error);
         }
       },
-    }));
+    })
+  );
+  builder.queryField("statisticalHistory", (t) =>
+    t.field({
+      type: [StatisticalHistoryRef],
+      args: {
+        start: t.arg({
+          type: "Date",
+          required: true,
+        }),
+        end: t.arg({
+          type: "Date",
+          required: true,
+        }),
+        metrics: t.arg({
+          type: [MetricHistoryInputRef],
+          required: true,
+        }),
+        interval: t.arg.string(),
+        samples: t.arg.int(),
+        raw: t.arg.boolean({ defaultValue: false }),
+      },
+      resolve: async (_parent, args) => {
+        const result = await getStatisticalHistory({ ...args, db });
+        if (isSuccess(result)) {
+          return result.output;
+        } else {
+          throw new GraphQLError(result.error);
+        }
+      },
+    })
+  );
 }
